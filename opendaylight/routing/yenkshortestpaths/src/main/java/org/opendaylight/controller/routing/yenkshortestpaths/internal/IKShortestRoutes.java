92c247eec2e889e26c870d20a39f4a42c6b586d3
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-07-14 17:06:47 +0200

    Fix KShortestRoutes interface

---
+++ opendaylight/routing/yenkshortestpaths/src/main/java/org/opendaylight/controller/routing/yenkshortestpaths/internal/IKShortestRoutes.java
@@ -0,0 +1,22 @@
+package org.opendaylight.controller.routing.yenkshortestpaths.internal;
+
+import java.util.List;
+
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.core.Path;
+
+public interface IKShortestRoutes {
+
+    /**
+     * Returns K shortest Paths leading from the source to the destination
+     *
+     *
+     * @param src
+     *            source {@link org.opendaylight.controller.sal.core.Node}
+     *
+     * @param dst
+     *            destination {@link org.opendaylight.controller.sal.core.Node}
+     * @return a List of {@link org.opendaylight.controller.sal.core.Path}
+     */
+    public List<Path> getKShortestRoutes(Node src, Node dst, int k);
+}
