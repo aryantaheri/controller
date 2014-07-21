01619a15bd852c3fb70393454c6bc2ed014ef9db
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-07-21 20:23:50 +0200

    Update according to OVSDB changes

--- opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/SouthboundInterfaceEvent.java
+++ opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/SouthboundInterfaceEvent.java
@@ -1,28 +1,28 @@
 package org.opendaylight.controller.samples.differentiatedforwarding.internal;
 
 import org.opendaylight.controller.sal.core.Node;
-import org.opendaylight.ovsdb.lib.table.internal.Table;
+import org.opendaylight.ovsdb.lib.notation.Row;
 import org.opendaylight.ovsdb.neutron.SouthboundEvent;
 
 public class SouthboundInterfaceEvent extends SouthboundEvent{
 
-    private Table<?> oldRow;
+    private Row oldRow;
 
     public SouthboundInterfaceEvent(Node node, String tableName, String uuid,
-            Table<?> row, Action action) {
+            Row row, Action action) {
         super(node, tableName, uuid, row, action);
     }
 
     public SouthboundInterfaceEvent(Node node, String tableName, String uuid,
-            Table<?> row, Object context, Action action) {
+            Row row, Object context, Action action) {
         super(node, tableName, uuid, row, context, action);
     }
 
-    public Table<?> getOldRow() {
+    public Row getOldRow() {
         return oldRow;
     }
 
-    public void setOldRow(Table<?> oldRow) {
+    public void setOldRow(Row oldRow) {
         this.oldRow = oldRow;
     }
 
