4880ed93ac0a723efaf1ecec94a02e99fa6bf185
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-08-01 19:58:26 +0200

    Removing simpleforwarding dependencies

--- opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/IForwarding.java
+++ opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/IForwarding.java
@@ -1,16 +1,10 @@
 package org.opendaylight.controller.samples.differentiatedforwarding;
 
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import org.opendaylight.controller.forwardingrulesmanager.FlowEntry;
-import org.opendaylight.controller.sal.core.NodeConnector;
-import org.opendaylight.controller.samples.simpleforwarding.HostNodePair;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 
 public interface IForwarding {
 
-    public ConcurrentMap<HostNodePair, HashMap<NodeConnector, FlowEntry>> getRulesDB();
+//    public ConcurrentMap<HostNodePair, HashMap<NodeConnector, FlowEntry>> getRulesDB();
     public Node getMdNode(String nodeDpId);
     public Long getExternalInterfaceOfPort(Node ofNode);
     public void programTunnelForwarding(Tunnel tunnel, int classNum, boolean write);
