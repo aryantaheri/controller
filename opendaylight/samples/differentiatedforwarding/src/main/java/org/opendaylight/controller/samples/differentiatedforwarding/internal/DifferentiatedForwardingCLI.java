2fb4c70c3330a2cec5edba23ccbf9d9fda51ac2d
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-09-05 12:46:09 +0200

    Clean up Diff CLI

--- opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/DifferentiatedForwardingCLI.java
+++ opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/DifferentiatedForwardingCLI.java
@@ -91,37 +91,5 @@ public class DifferentiatedForwardingCLI {
         System.out.println("OfPort: " + ofPort);
 
     }
-//    @Descriptor("Retrieves K shortest routes between two Nodes in the discovered Topology DB")
-//    public void getkShortestRoutes(
-//            @Descriptor("Container on the context of which the routing service need to be looked up") String container,
-//            @Descriptor("String representation of the Source Node, this need to be consumable from Node.fromString()") String srcNode,
-//            @Descriptor("String representation of the Destination Node") String dstNode,
-//            @Descriptor("Number of shortest paths (K)") int k) {
-//        System.out.println("YKShortestPathsCLI.getkShortestRoutes src:" + srcNode + " dst: " + dstNode + " K: " + k);
-//        IRouting r = null;
-//        Object[] rList = ServiceHelper.getInstances(IRouting.class, container, this, null);
-//        for (int i = 0; i < rList.length; i++) {
-//            if (rList[i] instanceof YKShortestPaths)
-//                r = (IRouting) rList[i];
-//        }
-//
-//        if (r == null) {
-//            System.out.println("Cannot find the YKSP routing instance on container:" + container);
-//            return;
-//        }
-//        System.out.println("YKShortestPathsCLI.getkShortestRoutes IRouting is retrieved: " + r.getClass());
-//        final Node src = Node.fromString(srcNode);
-//        final Node dst = Node.fromString(dstNode);
-//        final List<Path> paths = r.getKShortestRoutes(src, dst, k);
-//        if (paths != null) {
-//            System.out.println("K Shortest Routes between srcNode:" + src + " and dstNode:" + dst + " = " + paths);
-//            int i = 1;
-//            for (Path path : paths) {
-//                System.out.println("Route "+ i + ": " + path);
-//                i++;
-//            }
-//        } else {
-//            System.out.println("There is no route between srcNode:" + src + " and dstNode:" + dst);
-//        }
-//    }
+
 }
