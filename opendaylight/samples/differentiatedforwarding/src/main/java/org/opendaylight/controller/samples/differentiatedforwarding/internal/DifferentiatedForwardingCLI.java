361249c48ff0046fae186f5cc122284ae604bbee
Aryan TaheriMonfared <aryan.taherimonfared@uninett.no>
2014-09-05 15:14:39 +0200

    Use Src and Dst TEPs for creating/programming Tunnels

--- opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/DifferentiatedForwardingCLI.java
+++ opendaylight/samples/differentiatedforwarding/src/main/java/org/opendaylight/controller/samples/differentiatedforwarding/internal/DifferentiatedForwardingCLI.java
@@ -4,12 +4,14 @@ import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.felix.service.command.Descriptor;
 import org.opendaylight.controller.sal.utils.ServiceHelper;
 import org.opendaylight.controller.samples.differentiatedforwarding.IForwarding;
 import org.opendaylight.controller.samples.differentiatedforwarding.ITunnelObserver;
 import org.opendaylight.controller.samples.differentiatedforwarding.Tunnel;
+import org.opendaylight.controller.samples.differentiatedforwarding.TunnelEndPoint;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.osgi.framework.ServiceRegistration;
 
@@ -42,11 +44,11 @@ public class DifferentiatedForwardingCLI {
           @Descriptor("Tunnel key from getTunnelsMap") String tunnelKey,
           @Descriptor("Tunnel index from getTunnelsMap") int index,
           @Descriptor("Tunnel class (k in KShortestRoutes)") int classNum){
-        System.out.println("TenantTunnelObserver.getTunnelsMap");
+        System.out.println("DifferentiatedForwardingCLI - programTunnel");
 
         ITunnelObserver tunnelObserver = (ITunnelObserver) ServiceHelper.getInstance(ITunnelObserver.class, container, this);
         if(tunnelObserver == null){
-            System.err.println("TenantTunnelObserverCLI - getTunnelsMap: TenantTunnelObserver is not available");
+            System.err.println("DifferentiatedForwardingCLI - programTunnel: TenantTunnelObserver is not available");
             return;
         }
         IForwarding forwarding = (IForwarding) ServiceHelper.getInstance(IForwarding.class, container, this);
@@ -59,7 +61,41 @@ public class DifferentiatedForwardingCLI {
         Tunnel tunnel = tunnelsMap.get(tunnelKey).get(index);
         System.out.println("Chosen Tunnel:" + tunnel);
         forwarding.programTunnelForwarding(tunnel, classNum, true);
+    }
 
+    public void programTEPs(
+            @Descriptor("Container on the context of which the routing service need to be looked up") String container,
+            @Descriptor("Tunnel key from getTunnelEndPoints") String tunnelKey,
+            @Descriptor("Src TEP index from getTunnelEndPoints().get") int srcTepIndex,
+            @Descriptor("Dst TEP index from getTunnelEndPoints().get") int dstTepIndex,
+            @Descriptor("Tunnel class (k in KShortestRoutes)") int classNum){
+
+        System.out.println("DifferentiatedForwardingCLI - programTEPs");
+
+        ITunnelObserver tunnelObserver = (ITunnelObserver) ServiceHelper.getInstance(ITunnelObserver.class, container, this);
+        if(tunnelObserver == null){
+            System.err.println("DifferentiatedForwardingCLI - programTEPs: TenantTunnelObserver is not available");
+            return;
+        }
+        IForwarding forwarding = (IForwarding) ServiceHelper.getInstance(IForwarding.class, container, this);
+        if(forwarding == null){
+            System.err.println("DifferentiatedForwardingCLI - programTEPs: IForwarding is not available");
+            return;
+        }
+        HashMap<String, Set<TunnelEndPoint>> tunnelEndPointsMap = tunnelObserver.getTunnelEndPoints();
+        System.out.println(tunnelEndPointsMap);
+        TunnelEndPoint srcTep = tunnelEndPointsMap.get(tunnelKey).toArray(new TunnelEndPoint[0])[srcTepIndex];
+        TunnelEndPoint dstTep = tunnelEndPointsMap.get(tunnelKey).toArray(new TunnelEndPoint[0])[dstTepIndex];
+        System.out.println("Src TEP:" + srcTep);
+        System.out.println("Dst TEP:" + dstTep);
+        try {
+            Tunnel tunnel = null;
+            tunnel = new Tunnel(srcTep, dstTep);
+            System.out.println("Tunnel: " + tunnel);
+            forwarding.programTunnelForwarding(tunnel, classNum, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
